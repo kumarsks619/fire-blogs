fa12a256920ed9978060423d10b7a6437899e1e1
VeNoM <kumarsks619@gmail.com>
2022-05-14 14:53:45 +0530

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAFd5z92EHsjmkXB8eRSLhMUxScDGk-7Ho',
-    authDomain: 'fireblogs-sks.firebaseapp.com',
-    projectId: 'fireblogs-sks',
-    storageBucket: 'fireblogs-sks.appspot.com',
-    messagingSenderId: '54811026980',
-    appId: '1:54811026980:web:f286ab31b792c0cdfc1ab1',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
